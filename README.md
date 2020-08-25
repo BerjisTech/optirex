d034e5a3b360ecb41ed951f5dac14f53bdb2df6b
Benedict <berjistechnologies@gmail.com>
2020-08-26 01:25:02 +0300

    Laptop updates

--- README.md
+++
@@ -1,1 +0,0 @@
-bayanno-hospital-management-system
--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,9 +1,9 @@
 <?php $version = $this->db->get_where('settings', array('type' => 'version'))->row()->description;?>
 <!-- Footer -->
 <footer class="main">
-	&copy; 2017 <strong>Optirex Eye Care HMS</strong>
+	&copy; <?php echo date("Y"); ?> <strong>Optirex Eye Care HMS</strong>
     <strong class="pull-right"> VERSION <?php echo $version;?></strong>
     Developed by
-	<a href="http://creativeitem.com"
+	<a href="https://www.google.com/search?q=berjis+technologies"
     	target="_blank">Berjis Technologies</a>
 </footer>
